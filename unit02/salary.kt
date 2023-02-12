a9dfe7652dd96f8ef39959a576d3e32c546bdaba
samuelalgeda <122951490+samuelalgeda@users.noreply.github.com>
2023-02-12 10:57:18 -0600

    Create salary.kt

---
+++ unit02/salary.kt
@@ -0,0 +1,12 @@
+// Name: Samuel Algeda
+// Assignment: 02.4 Name, Age and Annual Income
+// Date: 2023-02-12
+
+fun main(args: Array<String>) {
+    val name = "Joe Mahoney"
+    val age = 26
+    val annualPay = 100000.0
+
+    println("My name is $name, my age is $age and")
+    println("I hope to earn \$$annualPay per year.")
+}
