46234800604434a1ef870406a9e3d176ff25bbd8
samuelalgeda <122951490+samuelalgeda@users.noreply.github.com>
2023-02-12 10:52:43 -0600

    Create marketing.kt

---
+++ unit02/marketing.kt
@@ -0,0 +1,13 @@
+// Name: Samuel Algeda
+// Assignment: 02.3 Advertising Conditional
+// Date: 2023-02-12
+
+fun main(args: Array<String>) {
+    val friendsCount = 500
+
+    if (friendsCount > 500) {
+        println("Adopt a Dog Today!")
+    } else {
+        println("Buy Cat Food At 20% Off")
+    }
+}
