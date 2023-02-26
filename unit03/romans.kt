f99884ba9a34acccc66684199bbdf6ddb3f27591
samuelalgeda <122951490+samuelalgeda@users.noreply.github.com>
2023-02-26 17:08:16 -0600

    Update romans.kt

--- unit03/romans.kt
+++ unit03/romans.kt
@@ -1,12 +1,14 @@
-fun main() {
-    print("Enter a number within the range of 1 through 10: ")
-    val number = readLine()?.toIntOrNull()
+import java.util.Scanner
 
-    if (number == null || number < 1 || number > 10) {
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    print("Enter a number within the range of 1 through 10: ")
+    val number = scanner.nextInt()
+
+    if (number < 1 || number > 10) {
         println("Error: Number must be within the range of 1 through 10.")
     } else {
         val romanNumerals = arrayOf("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X")
         println("The Roman numeral version of $number is ${romanNumerals[number - 1]}.")
     }
 }
-
