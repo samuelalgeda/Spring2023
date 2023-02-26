39b9b17ac99e39e3344df502b4704ba5709fcf9c
samuelalgeda <122951490+samuelalgeda@users.noreply.github.com>
2023-02-26 17:17:56 -0600

    Update magicNumbers.kt

--- unit03/magicNumbers.kt
+++ unit03/magicNumbers.kt
@@ -1,10 +1,9 @@
 import java.util.Scanner
 
 fun main() {
-    // Create a Scanner object for user input
+   
     val input = Scanner(System.`in`)
 
-    // Prompt the user to enter a date
     print("Enter the month (in numeric form): ")
     val month = input.nextInt()
     print("Enter the day: ")
@@ -12,7 +11,6 @@ fun main() {
     print("Enter the two-digit year: ")
     val year = input.nextInt()
 
-    // Check if the date is magic
     if (month * day == year) {
         println("The date is magic!")
     } else {
