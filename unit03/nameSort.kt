a115d8ae20c62fe60fd1fb17aa13b254d1c54b3a
samuelalgeda <122951490+samuelalgeda@users.noreply.github.com>
2023-03-09 09:23:39 -0600

    Update nameSort.kt

--- unit03/nameSort.kt
+++ unit03/nameSort.kt
@@ -1,12 +1,10 @@
 fun main() {
-    val input = Scanner(System.`in`)
-
     print("Enter the first name: ")
-    val name1 = input.nextLine()
+    val name1 = readLine()!!
     print("Enter the second name: ")
-    val name2 = input.nextLine()
+    val name2 = readLine()!!
     print("Enter the third name: ")
-    val name3 = input.nextLine()
+    val name3 = readLine()!!
 
     val names = listOf(name1, name2, name3).sorted()
 
